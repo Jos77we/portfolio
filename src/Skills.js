472a472bc60a81ca506acac49096ab9b07ac6b89
jos77we <josemaina207@gmail.com>
2023-06-23 14:51:07 +0300

    changes

--- src/Skills.js
+++ src/Skills.js
@@ -28,7 +28,7 @@ const Skills = () => {
         >
           Coding Languages
         </p>
-        <p style={{ color: "white", fontSize: "0.7rem", padding: "40px 10px" }}>
+        <p className="style-word">
           Coding languages that i do know are JavaScript, HTML, CSS and Java.I
           am able to write code that is easy to maintain and best performing
           when it comes time to deploy.
@@ -40,19 +40,13 @@ const Skills = () => {
             padding: "40px 0px 0px 10px",
             fontSize: "30px",
             color: "rgb(223, 13, 13)",
+            
           }}
         />
-        <p
-          style={{
-            color: "white",
-            fontSize: "1.2rem",
-            marginLeft: "10px",
-            maxWidth: "100px",
-          }}
-        >
+        <p style={{ color: "white", fontSize: "1.2rem", marginLeft: "10px", maxWidth: "100px" }}>
           Front-End Development
         </p>
-        <p style={{ color: "white", fontSize: "0.7rem", padding: "40px 10px" }}>
+        <p className="style-word">
           Front-End Development involves how users get to interact with a
           website or app. Having components like animations, interactive chat
           response and a good landing page with amazing color sheme and font
@@ -67,17 +61,10 @@ const Skills = () => {
             color: "rgb(223, 13, 13)",
           }}
         />
-        <p
-          style={{
-            color: "white",
-            fontSize: "1.2rem",
-            marginLeft: "10px",
-            maxWidth: "100px",
-          }}
-        >
+        <p style={{ color: "white", fontSize: "1.2rem", marginLeft: "10px", maxWidth: "100px" }}>
           Back-End Development
         </p>
-        <p style={{ color: "white", fontSize: "0.7rem", padding: "40px 10px" }}>
+        <p className="style-word">
           In Back-End Development it involves rendering your project on a
           server, making API endpounts, storing data for your project and even
           cloud development. I can make resposive APIs both in Java or
@@ -102,7 +89,7 @@ const Skills = () => {
         >
           Mobile Development
         </p>
-        <p style={{ color: "white", fontSize: "0.7rem", padding: "40px 10px" }}>
+        <p className="style-word">
           Mobile apps are a booming industry which makes users able to acces
           different services from there phones. Having expereience with Java and
           JavaScript I can make apps for android.
@@ -126,7 +113,7 @@ const Skills = () => {
         >
           UI/UX Design
         </p>
-        <p style={{ color: "white", fontSize: "0.7rem", padding: "40px 10px" }}>
+        <p className="style-word">
           Designing of your landing page or Mobile app help you in deciding on
           what you woukd like to get or what you would want your user to
           expereience whenever they do want to interact with your services
